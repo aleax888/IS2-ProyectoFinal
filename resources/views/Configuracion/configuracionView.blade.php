40fd99ae8bbba94be24ad744d443dc9e50b49f1f
Andres Cusirramos Marquez Mares <49249500+Andrescmm@users.noreply.github.com>
2021-12-07 21:51:26 -0500

    configuracion completo

--- resources/views/Configuracion/configuracionView.blade.php
+++ resources/views/Configuracion/configuracionView.blade.php
@@ -27,32 +27,54 @@
 @endsection
 
 @section('content')
-    <h1>Configuracion</h1>
+<div style="display:inline">
+    <div class="container" style= "float:left; background-color:#eef2f9; " width="50%">
+                    <div class="text-center" style="margin-top:100px">
+                        <h2 class="section-heading text-uppercase">EVENTOS</h2>
+                    </div>
+                    <div class="row">
+                
+                @foreach ($t as $evento)
+                <div class="col-lg-4">
+                            <div class="team-member">
+                             <img class="img-thumbnail" width="330px" heigth="330px" src='../images/{{$evento->id}}.jpeg' alt="..." />
+                             <h3><b>{{$evento->nombre}}</b></h3>
+        
+                    </div>
+                </div>
+                @endforeach
+                    </div>
+    </div>
+</div>   
+<div style = "float:right; background-color:#c7dede; margin-top:40px" width="10%" > 
+    <h1><b>Configuracion</b></h1>
 
     <a href="{{url('configuracion/crearevento')}}">
         <h3>Crear Evento</h3>
     </a> 
     
     <h3>Adaptar Evento</h3>
-    <div style="width:50%">
-    <table class="table table-striped table-light" border="1">
+    <div style="width:750px">
+    <table class="table table-striped table-light" border="1" width= "100%%" style="border-color:#c7dede; border: 1px solid #c7dede;">
         <tr>
-            <th>Eventos</th>
+            <th style ="width: 33%;text-align: left; vertical-align: top;border: 1px solid #c7dede;">Eventos</th>
+            <th  style ="width: 33%;text-align: left; vertical-align: top;border: 1px solid #c7dede;"></th>
+            <th  style ="width: 33%;text-align: left; vertical-align: top;border: 1px solid #c7dede;"></th>
         </tr>
                 
         @foreach ($t as $t)
             <tr>
-                <td>
-                    {{$t->nombre}}
+                <td style ="width: 33%;text-align: left; vertical-align: top;border: 1px solid #c7dede;">
+                   <b>{{$t->nombre}}</b>
                 </td>
-                <td>
+                <td style ="width: 33%;text-align: left; vertical-align: top;border: 1px solid #c7dede;">
                     <a href="{{url('configuracion/adaptarevento')}}">
-                        adaptar
+                        <b>Adaptar</b>
                     </a>
                 </td>
-                <td>
+                <td style ="width: 33%;text-align: left; vertical-align: top;border: 1px solid #c7dede;">
                     <a href="{{url('configuracion/editarevento')}}">
-                        editar
+                        <b>Editar</b>
                     </a>
                 </td>
             </tr>
@@ -71,5 +93,13 @@
     <a href="#">
         <h3>Nuevo Material</h3>
     </a> 
+</div>
 
+<footer style="display: table;
+    text-align: center;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 45%">
+    <p>© DevCode-2021 Todos los derechos reservados UCSP</p>
+</footer>
 @endsection
--- resources/views/Reportes/reportesView.blade.php
+++ resources/views/Reportes/reportesView.blade.php
@@ -68,7 +68,7 @@
                 <tr style ="width: 25%;text-align: left; vertical-align: top;border: 1px solid #c7dede;">
                     <td style ="width: 25%;text-align: left; vertical-align: top;border: 1px solid #c7dede;">
                         <a href="{{url('reportes/cierredecaja/evento/' . $t->id)}}">
-                            {{$t->nombre}}
+                           <b>{{$t->nombre}}</b>
                         </a>
                     </td>
                 </tr>
